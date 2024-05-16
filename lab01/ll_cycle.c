6096881cd5e7636e26c6c8025e5797fc12c8af34
PeterWang <peterwangharuko@gmail.com>
2024-05-17 00:36:33 +0800

    complete lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise, *hare;
+    tortoise = head;
+    hare = head;
+    while (hare && (*hare).next) {
+        hare = (*(*hare).next).next;
+        tortoise = (*tortoise).next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
