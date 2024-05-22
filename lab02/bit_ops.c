de8517148e120de59b9568567cecd34226eb947e
PeterWang <peterwangharuko@gmail.com>
2024-05-22 11:34:15 +0800

    Complete lab2

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,6 +9,9 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
+    x = x >> n;
+	unsigned result = x & 1;
+	return result;
     return -1;
 }
 // Set the nth bit of the value of x to v.
@@ -17,11 +20,18 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+	unsigned helper1 = ~(1 << n);
+	*x = *x & helper1;
+	unsigned helper2 = v << n;
+	*x = *x | helper2;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+	unsigned tem = 1;
+	tem = tem << n;
+	*x = *x ^ tem;
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,13 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t n0 = (*reg >> 0) & 1;
+    uint16_t n2 = (*reg >> 2) & 1;
+    uint16_t n3 = (*reg >> 3) & 1;
+    uint16_t n5 = (*reg >> 5) & 1;
+    uint16_t next = n0 ^ n2 ^ n3 ^ n5;
+    *reg = *reg >> 1;
+    next = next << 15;
+    *reg = *reg | next;
 }
 
