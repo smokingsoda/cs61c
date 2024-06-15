1d55489bf83afaeade5f5a7a9746c0d5c27f7d95
PeterWang <peterwangharuko@gmail.com>
2024-06-15 17:53:28 +0800

    Handle control hazard

--- proj3/tests/part_b/custom/inputs/wjr0.s
+++ proj3/tests/part_b/custom/inputs/wjr0.s
@@ -1,7 +1,16 @@
 add t0, x0, x0
-addi t1, x0, 100
+addi t1, x0, 20
 add t2, x0, x0
-Label:
+Label1:
 add t2, t1, t2
 addi t0, t0, 1
-bne t0, t1, Label
+blt t0, t1, Label1
+addi s0, s0, 1
+add t0, x0, x0
+Label2:
+add t2, t0, t2
+addi t0, t0, 1
+blt t0, t1, Label2
+mul t2, t2, t1
+sub t2, t2, t1
+
