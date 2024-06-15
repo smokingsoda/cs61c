1ef267e503f05e5182fbbd03dea8a8276429049c
PeterWang <peterwangharuko@gmail.com>
2024-06-15 13:31:22 +0800

    Debugging SB_Type and PC

--- proj3/tests/part_b/custom/inputs/wjr0.s
+++ proj3/tests/part_b/custom/inputs/wjr0.s
@@ -1,7 +1,7 @@
-addi t0, x0, 10
-addi t1, x0, 20
-mul t2, t0, t1
-addi sp, sp, -12
-sw t2, 0(sp)
-sw t1, 4(sp)
-sw t0, 8(sp)
+add t0, x0, x0
+addi t1, x0, 10
+add t2, x0, x0
+Label:
+add t2, t1, t2
+addi t0, t0, 1
+blt t0, t1, Label
