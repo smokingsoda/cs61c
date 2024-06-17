a6e3d2227e5965d74ceb2f61cb627d4223d1b7f8
PeterWang <peterwangharuko@gmail.com>
2024-06-17 11:50:43 +0800

    Complete loads and jalr

--- proj3/tests/part_b/custom/inputs/wjr0.s
+++ proj3/tests/part_b/custom/inputs/wjr0.s
@@ -1,5 +1,14 @@
-jal ra label
-addi s0 x0 -1
-jal x0 end
-label: jal x0 -8
-end: addi a0 x0 -1
+addi t0, x0, 10
+addi t1, x0, 20
+addi t2, x0, 30
+addi sp, sp, -12
+sw t0, 0(sp)
+sw t1, 4(sp)
+sw t2, 8(sp)
+add t0, x0, x0
+add t1, x0, x0
+add t2, x0, x0
+lw t0, 0(sp)
+lw t1, 4(sp)
+lw t2, 8(sp)
+addi sp, sp, 12
