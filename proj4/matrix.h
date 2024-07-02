7c905869e89a0a1e5e09bd2a3fcf638c147388b0
PeterWang <peterwangharuko@gmail.com>
2024-07-02 16:57:20 +0800

    Modify include for server

--- proj4/matrix.h
+++ proj4/matrix.h
@@ -1,5 +1,5 @@
-#include "/opt/homebrew/Cellar/python@3.12/3.12.4/Frameworks/Python.framework/Versions/3.12/include/python3.12/Python.h"
-//#include <Python.h>
+//#include "/opt/homebrew/Cellar/python@3.12/3.12.4/Frameworks/Python.framework/Versions/3.12/include/python3.12/Python.h"
+#include <Python.h>
 
 typedef struct matrix {
     int rows;      	// number of rows
