565adc5f2cf8b115adec8e5619d3e1e685e6339e
PeterWang <peterwangharuko@gmail.com>
2024-07-02 16:00:07 +0800

    Modify setup.py

--- proj4/setup.py
+++ proj4/setup.py
@@ -1,24 +1,14 @@
-from distutils.core import (setup, Extension)
-import os
+from distutils.core import setup, Extension
 import sysconfig
 
 def main():
-    os.environ["CC"] = "gcc-14"  # 设置环境变量 CC 为 gcc-14
-    CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp' , '-pthread', '-O3']
+    CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
     LDFLAGS = ['-fopenmp']
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # TODO: YOUR CODE HERE
-    setup(name="numc",
-          version="0.0.1",
-          description="numc matrix operations",
-          ext_modules=[
-            Extension("numc",
-                      sources=["numc.c", "matrix.c"],
-                      extra_compile_args=CFLAGS,
-                      extra_link_args=LDFLAGS,
-                      language='c')
-          ])
+    module = Extension('numc', sources=['numc.c', 'matrix.c'], extra_compile_args=CFLAGS, extra_link_args=LDFLAGS)
 
+    setup(name='numc', version='1.0', ext_modules=[module]); 
 if __name__ == "__main__":
     main()
