8887d1618bea3a8ff2b2a5090ca6543c9fc196fe
PeterWang <peterwangharuko@gmail.com>
2024-07-06 19:40:35 +0800

    SegFault fix

--- lab09/simd.c
+++ lab09/simd.c
@@ -103,10 +103,10 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
 		__m128i and_flag3;
 		int inner_sum_array[4] = {0, 0, 0, 0};
 		for (unsigned int i = 0; i < NUM_ELEMS; i += 16) {
-			elements0 = _mm_loadu_si128((__m128i*)(vals + i + 0 ));
-			elements1 = _mm_loadu_si128((__m128i*)(vals + i + 4 * i));
-			elements2 = _mm_loadu_si128((__m128i*)(vals + i + 8 * i));
-			elements3 = _mm_loadu_si128((__m128i*)(vals + i + 12 * i));
+			elements0 = _mm_loadu_si128((__m128i*)(vals + i));
+			elements1 = _mm_loadu_si128((__m128i*)(vals + i + 4));
+			elements2 = _mm_loadu_si128((__m128i*)(vals + i + 8));
+			elements3 = _mm_loadu_si128((__m128i*)(vals + i + 12));
 			and_flag0 = _mm_cmpgt_epi32(elements0, _127);
 			and_flag1 = _mm_cmpgt_epi32(elements1, _127);
 			and_flag2 = _mm_cmpgt_epi32(elements2, _127);
