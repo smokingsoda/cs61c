7875ce74af068ab263db3af06bfffdbeed23146f
PeterWang <peterwangharuko@gmail.com>
2024-07-10 12:02:34 +0800

    Collapse fix

--- proj4/matrix.c
+++ proj4/matrix.c
@@ -186,19 +186,22 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     if (mat1->rows != mat2->rows || mat1->cols != mat2->cols || result->rows != mat1->rows || result->cols != mat1->cols) {
         return -1;
     }
+    int rows = result->rows;
+    int cols = result->cols;
+    int boundary = cols / 4 * 4
     __m256d result_element;
     __m256d mat1_element;
     __m256d mat2_element; //256 bit can contain 4 double
     #pragma omp parallel for collapse(2)
         for (int i = 0; i < result->rows; i++) {
-            for (int j = 0; j < result->cols / 4 * 4; j+=4) {
+            for (int j = 0; j < boundary; j+=4) {
                 mat1_element = _mm256_loadu_pd(&(mat1->data[i][j]));
                 mat2_element = _mm256_loadu_pd(&(mat2->data[i][j]));
                 result_element = _mm256_add_pd(mat1_element, mat2_element);
                 _mm256_storeu_pd(&(result->data[i][j]), result_element);
                 //*(*(result->data + i) + j) = *(*(mat1->data + i) + j) + *(*(mat2->data + i) + j);
             }
-            for (int j = result->cols / 4 * 4; j < result->cols; j++) {
+            for (int j = boundary; j < cols; j++) {
                 result->data[i][j] = mat1->data[i][j] + mat2->data[i][j];
             }
         }
