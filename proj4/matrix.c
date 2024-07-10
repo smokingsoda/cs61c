e59f8510dd34904e1ef1478d85e03b30125f5e92
PeterWang <peterwangharuko@gmail.com>
2024-07-10 11:05:02 +0800

    No cast type and find bug

--- proj4/matrix.c
+++ proj4/matrix.c
@@ -190,11 +190,11 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     __m256d mat1_element;
     __m256d mat2_element; //256 bit can contain 4 double
     for (int i = 0; i < result->rows; i++) {
-        for (int j = 0; j < result->cols; j+=4) {
+        for (int j = 0; j < result->cols / 4 * 4; j+=4) {
             mat1_element = _mm256_loadu_pd(&(mat1->data[i][j]));
             mat2_element = _mm256_loadu_pd(&(mat2->data[i][j]));
             result_element = _mm256_add_pd(mat1_element, mat2_element);
-            _mm256_storeu_pd((__m256d*)&(result->data[i][j]), result_element);
+            _mm256_storeu_pd(&(result->data[i][j]), result_element);
             //*(*(result->data + i) + j) = *(*(mat1->data + i) + j) + *(*(mat2->data + i) + j);
         }
         for (int j = result->cols / 4 * 4; j < result->cols; j++) {
