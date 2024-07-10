07dd735707e31a1b63887872057be459ef40670a
PeterWang <peterwangharuko@gmail.com>
2024-07-10 11:26:24 +0800

    Add openMP again

--- proj4/matrix.c
+++ proj4/matrix.c
@@ -189,18 +189,19 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     __m256d result_element;
     __m256d mat1_element;
     __m256d mat2_element; //256 bit can contain 4 double
-    for (int i = 0; i < result->rows; i++) {
-        for (int j = 0; j < result->cols / 4 * 4; j+=4) {
-            mat1_element = _mm256_loadu_pd(&(mat1->data[i][j]));
-            mat2_element = _mm256_loadu_pd(&(mat2->data[i][j]));
-            result_element = _mm256_add_pd(mat1_element, mat2_element);
-            _mm256_storeu_pd(&(result->data[i][j]), result_element);
-            //*(*(result->data + i) + j) = *(*(mat1->data + i) + j) + *(*(mat2->data + i) + j);
+    #pragma omp parallel for
+        for (int i = 0; i < result->rows; i++) {
+            for (int j = 0; j < result->cols / 4 * 4; j+=4) {
+                mat1_element = _mm256_loadu_pd(&(mat1->data[i][j]));
+                mat2_element = _mm256_loadu_pd(&(mat2->data[i][j]));
+                result_element = _mm256_add_pd(mat1_element, mat2_element);
+                _mm256_storeu_pd(&(result->data[i][j]), result_element);
+                //*(*(result->data + i) + j) = *(*(mat1->data + i) + j) + *(*(mat2->data + i) + j);
+            }
+            for (int j = result->cols / 4 * 4; j < result->cols; j++) {
+            r   esult->data[i][j] = mat1->data[i][j] + mat2->data[i][j];
+            }
         }
-        for (int j = result->cols / 4 * 4; j < result->cols; j++) {
-            result->data[i][j] = mat1->data[i][j] + mat2->data[i][j];
-        }
-    }
     return 0;
 }
 
