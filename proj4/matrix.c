f806f2de2c18b61275177309ca789ce0b7da9eed
PeterWang <peterwangharuko@gmail.com>
2024-07-10 14:50:48 +0800

    Debug

--- proj4/matrix.c
+++ proj4/matrix.c
@@ -220,7 +220,7 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     #pragma omp parallel for collapse(2)
     for (int i = 0; i < rows; i++) {
         for (int j = boundary; j < cols; j++) {
-            result->data[i][j] = mat1->data[i][j];
+            result->data[i][j] = mat1->data[i][j] + mat2->data[i][j];
         }
     }
     return 0;
