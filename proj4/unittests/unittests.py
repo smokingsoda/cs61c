d58164df2d6825c9a1d920fb023bc624af45345e
PeterWang <peterwangharuko@gmail.com>
2024-07-10 10:15:58 +0800

    Decrease matrix size

--- proj4/unittests/unittests.py
+++ proj4/unittests/unittests.py
@@ -25,8 +25,8 @@ class TestAdd(TestCase):
 
     def test_large_add(self):
         # TODO: YOUR CODE HERE
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(1000000, 1000000, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(1000000, 1000000, seed=1)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(10000, 10000, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(10000, 10000, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
@@ -50,8 +50,8 @@ class TestSub(TestCase):
 
     def test_large_sub(self):
         # TODO: YOUR CODE HERE
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(1000000, 1000000, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(1000000, 1000000, seed=1)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(10000, 10000, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(10000, 10000, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
@@ -73,7 +73,7 @@ class TestAbs(TestCase):
 
     def test_large_abs(self):
         # TODO: YOUR CODE HERE
-        dp_mat, nc_mat = rand_dp_nc_matrix(1000000, 1000000, seed=0)
+        dp_mat, nc_mat = rand_dp_nc_matrix(10000, 10000, seed=0)
         is_correct, speed_up = compute([dp_mat], [nc_mat], "abs")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
