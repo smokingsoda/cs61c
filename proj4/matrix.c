b31a7e826032a380814903bd11e794dbcb110431
PeterWang <peterwangharuko@gmail.com>
2024-07-11 11:23:37 +0800

    Try cache blocksize

--- proj4/matrix.c
+++ proj4/matrix.c
@@ -195,7 +195,7 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     __m256d mat2_element0, mat2_element1, mat2_element2, mat2_element3; //256 bit can contain 4 double
     //#pragma omp parallel for collapse(2)
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < boundary; j+=16) {
+            for (int j = 0; j < boundary; j+=8) {
                 mat1_element0 = _mm256_loadu_pd(&(mat1->data[i][j]));
                 mat1_element1 = _mm256_loadu_pd(&(mat1->data[i][j + 4]));
 
