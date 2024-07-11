cbdf01ecbe3c5107d4cfb69a6cff334ddeaae63e
PeterWang <peterwangharuko@gmail.com>
2024-07-11 12:10:57 +0800

    64 byte cache size

--- proj4/matrix.c
+++ proj4/matrix.c
@@ -195,14 +195,18 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     __m256d mat2_element0, mat2_element1, mat2_element2, mat2_element3; //256 bit can contain 4 double
     //#pragma omp parallel for collapse(2)
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < boundary; j+=4) {
+            for (int j = 0; j < boundary; j+=8) {
                 mat1_element0 = _mm256_load_pd((void*)&(mat1->data[i][j]));
+                mat1_element1 = _mm256_load_pd((void*)&(mat1->data[i][j + 4]));
 
                 mat2_element0 = _mm256_load_pd((void*)&(mat2->data[i][j]));
+                mat2_element1 = _mm256_load_pd((void*)&(mat2->data[i][j + 4]));
 
                 result_element0 = _mm256_add_pd(mat1_element0, mat2_element0);
+                result_element1 = _mm256_add_pd(mat1_element1, mat2_element1);
 
                 _mm256_store_pd((void*)&(result->data[i][j]), result_element0);
+                _mm256_store_pd((void*)&(result->data[i][j + 4]), result_element1);
                 //*(*(result->data + i) + j) = *(*(mat1->data + i) + j) + *(*(mat2->data + i) + j);
             }
         }
