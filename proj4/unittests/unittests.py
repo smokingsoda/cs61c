05445174a3dbcf791b6f92798de8f98131416f31
PeterWang <peterwangharuko@gmail.com>
2024-07-11 16:42:51 +0800

    Modify size

--- proj4/unittests/unittests.py
+++ proj4/unittests/unittests.py
@@ -1,8 +1,8 @@
 from utils import *
 from unittest import TestCase
 small = 4
-medium = 40
-large = 400
+medium = 400
+large = 1600
 pow_times = 3
 """
 For each operation, you should write tests to test  on matrices of different sizes.
