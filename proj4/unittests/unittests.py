6f21ff6132e2fb2402a8ca87b80ecf6cc7eab2a9
PeterWang <peterwangharuko@gmail.com>
2024-07-12 12:50:19 +0800

    Test odd other

--- proj4/unittests/unittests.py
+++ proj4/unittests/unittests.py
@@ -132,8 +132,8 @@ class TestMul(TestCase):
 
     def test_odd_mul(self):
         # TODO: YOUR CODE HERE
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(5, 7, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(7, 9, seed=1)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(5, 6, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(6, 1003, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
